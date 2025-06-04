6af53f12afbff3392c114c0b4172afa750b3c2d5
KusumaVardhini <ramaiahgarikusuma@gmail.com>
2025-06-04 10:24:49 +0530

    Add files via upload

---
+++ palindromeusingfunctions.java
@@ -0,0 +1,17 @@
+class palindromeusingfunctions {
+	public static int ispalindrome(String s) {
+		int left=0;
+		int right=s.length()-1;
+		while(left<right){
+			if(s.charAt(left)!=s.charAt(right))
+			return 0;
+			left++;
+			right--;
+		}
+		return 1;
+	}
+	public static void main(String[] args){
+        String s="abba";
+        System.out.println(ispalindrome(s));
+	}
+}	

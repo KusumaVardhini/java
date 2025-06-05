ce66cdf9ecce34c22e81630f0acdcd423aba5ec2
KusumaVardhini <ramaiahgarikusuma@gmail.com>
2025-06-05 16:00:52 +0530

    Add files via upload

---
+++ BeautySum.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class BeautySum {
+    public int beautySum(String s) {
+        int ans = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int[] freq = new int[26];
+            for (int j = i; j < s.length(); j++) {
+                char ch = s.charAt(j);
+                freq[ch - 'a']++;
+                int max = Integer.MIN_VALUE;
+                int min = Integer.MAX_VALUE;
+                for (int k = 0; k < 26; k++) {
+                    if (freq[k] > 0) {
+                        min = Math.min(min, freq[k]);
+                        max = Math.max(max, freq[k]);
+                    }
+                }
+                ans += max - min;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        BeautySum sol = new BeautySum();
+        int result = sol.beautySum(input);
+        System.out.println("Beauty sum of the string: " + result);
+    }
+}
